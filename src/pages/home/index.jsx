c275f5cb23d4c53481d7d59f989a0b6db42ec689
Satish Singh <satish99bkp@gmail.com>
2024-02-01 16:26:00 +0530

    Second commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import style from "./style.module.scss";
 // import Modal from "../../components/modal";
 import LoginModal from "../../components/loginModal";
+import {useState} from 'react';
 
 const Home = () => {
+  const [openState, setOpenModal]=useState(false);
+  const openModal=()=>{
+    if(openState===false){
+      setOpenModal(true);
+    }
+  }
+  
   return (
     <div className={style.page}>
-      <button className="primary"> Open Modal</button>
-      <LoginModal />
+      <button className="primary" onClick={openModal}> Open Modal</button>
+      {
+        openState && <LoginModal/>
+      }
+      
     </div>
   );
 };
