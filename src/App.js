dbd4cb7d9767147e36f56d7945b0d8b57169964e
Satish Singh <satish99bkp@gmail.com>
2024-02-12 23:27:01 +0530

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import HomePage from "./pages/doctorwebsite/home";
+ import HomePage from "./pages/doctorwebsite/home";
 // import Item from "./components/cart/Item";
 // import Cart from "./components/cart/Cart";
-
+  // import Home from "./pages/home";
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
         <Item name="iPhone Pro" price={400}/>
         <Item name="PenDrive" price={50}/>
         <Cart/> */}
-        
+        {/* <Home /> */}
       
     </div>
   );
